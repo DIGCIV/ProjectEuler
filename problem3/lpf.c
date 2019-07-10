367d55cae4a994af20a57000577554a3e6696249
DIGCIV <zhurbenkoalexander@gmail.com>
2019-07-10 21:33:16 +0300

    a.out delleted, comments to files *.c added

--- problem3/lpf.c
+++ problem3/lpf.c
@@ -1,25 +1,29 @@
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+*/
+
 #include <stdio.h>
 #include <math.h>
 
-int		main(void)
+int	main(void)
 {
-		long	lp, i, num, num1;
+	long	lp, i, num;
 
-		lp = 2;
-		num = 600851475143;
-		num1 = 13195;
-		i = 5;
-//		while ((num % 2) == 0)
-//				num /= 2;
-		while (i <= (num))
+	lp = 2;
+	num = 600851475143;
+	i = 5;
+	while ((num % 2) == 0)
+		num /= 2;
+	while (i <= (num))
+	{
+		if (num % i == 0)
 		{
-				if (num % i == 0)
-				{
-						lp = i;
-						num /= i;
-				}
-				i += 2;
+			lp = i;
+			num /= i;
 		}
-		printf("%ld", lp);
-		return 0;
+		i += 2;
+	}
+	printf("%ld\n", lp);
+	return 0;
 }
---
+++ problem4/largestPP.c
@@ -0,0 +1,17 @@
+/*
+ *A palindromic number reads the same both ways.
+ *The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ *Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+#include "stdio.h"
+
+int main()
+{
+    int first_3digit_num, second_3digit_num, res;
+    
+    first_3digit_num = 999;
+    second_3digit_num = 999;
+    res = first_3digit_num * second_3digit_num;
+    
+}
