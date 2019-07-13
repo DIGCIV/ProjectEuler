7b9b8a8d9e755d03358d93847738f7a0ad6d6ef0
DIGCIV <zhurbenkoalexander@gmail.com>
2019-07-13 18:55:55 +0300

    solved problems 1 - 9, added comments to all .c files

--- problem4/largestPP.c
+++ problem4/largestPP.c
@@ -4,14 +4,48 @@
  *Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-#include "stdio.h"
+#include <stdio.h>
+
+int isPalindrome(int numb)
+{
+    int buf, rev;
+
+    buf = numb;
+    rev = 0;
+    while (buf / 10 > 0)
+    {
+        rev = rev * 10 + buf % 10;
+        buf = buf / 10;
+    }
+    rev = rev * 10 + buf % 10;
+    if (numb == rev)
+    {
+        return (1);
+    }
+    return (0);
+}
 
 int main()
 {
-    int first_3digit_num, second_3digit_num, res;
+    int first_3digit_num, second_3digit_num, res, pal;
     
     first_3digit_num = 999;
     second_3digit_num = 999;
-    res = first_3digit_num * second_3digit_num;
-    
+    pal = 0;
+    while (first_3digit_num >= 100)
+    {
+        if (second_3digit_num < 100)
+        {
+            first_3digit_num--;
+            second_3digit_num = 999;
+        }
+        res = first_3digit_num * second_3digit_num;
+        if (isPalindrome(res) && res > pal)
+        {
+            pal = res;
+        }
+        second_3digit_num--;
+    }
+    printf("%d\n", pal);
+    return (0);
 }
--- problem9/pythtrip.c
+++ problem9/pythtrip.c
@@ -1,2 +1,28 @@
+/*
+ *A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *a^2 + b^2 = c^2
+ *For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ *Find the product abc.
+*/
+
 #include <stdio.h>
 
+int main()
+{
+    int m, n;
+
+    m = 2;
+    n = 1;
+    while (m * (m + n) != 500)
+    {
+        n++;
+        if (n == m)
+        {
+            m++;
+            n = 1;
+        }
+    }
+    printf("%d\n", 2 * m * n * (m * m - n * n) * (m * m + n *n));
+    return (0);
+}
